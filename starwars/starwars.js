b194ed68aa1007352547076bc20ee3cbd508ade2
Kiera Summers <10262861@my.uvu.edu>
2021-03-25 09:48:20 -0600

    update picture to make loop

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -9,7 +9,7 @@ for (let i = 0; i < films.length; i++) {
     const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
     let posterFig = document.createElement('figure')
     let figImg = document.createElement('img')
-    figImg.src = 'https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg'
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
     let figCaption = document.createElement('figcaption')
 
     figCaption.textContent = foundFilm.title
