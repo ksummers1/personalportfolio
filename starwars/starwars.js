00f8bb9d81cd2f40b8d34d891c2deb75d5e246d3
Kiera Summers <10262861@my.uvu.edu>
2021-03-25 02:56:57 -0600

    update coding for film order

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -5,10 +5,8 @@ let filmList = document.querySelector('#filmList')
 
 
 for (let i = 0; i < films.length; i++) {
-    console.log(`the loop counter is: ${i} while the film episode id is: ${films[i].episode_id}`)
 
-    //let lastNum = getLastNumber(films[i].url)
-    const foundFilm - films.find(film => getLastNumber(film.url) === (i + 1).toString())
+    const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
     let filmItem = document.createElement('li')
     filmItem.textContent = foundFilm.title
     filmList.appendChild(filmItem)
@@ -17,5 +15,3 @@ for (let i = 0; i < films.length; i++) {
 function getLastNumber(url) {
     let end = url.lastIndexOf('/')
     return url.charAt(end - 1)
-        //console.log(`filmNum is ${url.charAt(filmNum)}`)
-}
