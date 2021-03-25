61ffbc6f24280057bc796abfbd650e63301bc2bf
Kiera Summers <10262861@my.uvu.edu>
2021-03-25 02:08:07 -0600

    update js coding

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -5,7 +5,14 @@ let filmList = document.querySelector('#filmList')
 
 
 for (let i = 0; i < films.length; i++) {
+    console.log(`the loop counter is: ${i} while the film episode id is: ${films[i].episode_id}`)
     let filmItem = document.createElement('li')
     filmItem.textContent = films[i].title
     filmList.appendChild(filmItem)
+    getLastNumber(films[i].url)
+}
+
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    console.log(`last character is index#${end}`)
 }
