43f92deb9fbf5b91199d0c4467a383aa82e347d6
Kiera Summers <10262861@my.uvu.edu>
2021-03-25 01:48:25 -0600

    update to film coding

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -1,36 +1,11 @@
 import { films } from '../data/films.js'
 
 
-console.log(films[6])
+let filmList = document.querySelector('#filmList')
 
-let filmOne = document.querySelector('#film1')
 
-let filmTwo = document.querySelector('#film2')
-
-let filmThree = document.querySelector('#film3')
-
-let filmFour = document.querySelector('#film4')
-
-let filmFive = document.querySelector('#film5')
-
-let filmSix = document.querySelector('#film6')
-
-let filmSeven = document.querySelector('#film7')
-
-filmOne.textContent = films[0].title
-
-filmTwo.textContent = films[1].title
-
-filmThree.textContent = films[2].title
-
-filmFour.textContent = films[3].title
-
-filmFive.textContent = films[4].title
-
-filmSix.textContent = films[5].title
-
-filmSeven.textContent = films[6].title
-
-for (var i = 0; i < films.length; i++) {
-    console.log(films[i]);
+for (let i = 0; i < films.length; i++) {
+    let filmItem = document.createElement('li')
+    filmItem.textContent = films[i].title
+    filmList.appendChild(filmItem)
 }
