213fb64c4d404724f059c2c820732ab3763e42f3
Kiera Summers <10262861@my.uvu.edu>
2021-03-25 01:05:51 -0600

    add more movie titles

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -5,4 +5,28 @@ console.log(films[5])
 
 let filmOne = document.querySelector('#film1')
 
+let filmTwo = document.querySelector('#film2')
+
+let filmThree = document.querySelector('#film3')
+
+let filmFour = document.querySelector('#film4')
+
+let filmFive = document.querySelector('#film5')
+
+let filmSix = document.querySelector('#film6')
+
+let filmSeven = document.querySelector('#film7')
+
 filmOne.textContent = films[0].title
+
+filmOne.textContent = films[1].title
+
+filmOne.textContent = films[2].title
+
+filmOne.textContent = films[3].title
+
+filmOne.textContent = films[4].title
+
+filmOne.textContent = films[5].title
+
+filmOne.textContent = films[6].title
