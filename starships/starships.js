84593c2fb7b19b7c08a61c646fe833ef2ea973b1
Kiera Summers <10262861@my.uvu.edu>
2021-03-26 05:15:25 -0600

    working on getting ships to work

--- starships/starships.js
+++ starships/starships.js
@@ -7,19 +7,35 @@ const navList = document.querySelector('.navList')
 const shipView = document.querySelector('.shipView')
 
 function populateNav(starships) {
-    starships.forEach(starship => {
+    starships.forEach((starship) => {
+        let anchorWrap = document.createElement('a')
+        anchorWrap.href = '#'
+        anchorWrap.addEventListener('click', () => populateShipView(starship))
         let listItem = document.createElement('li')
         listItem.textContent = starship.name
 
+        anchorWrap.appendChild(listItem)
         navList.appendChild(listItem)
     })
 
 }
 
 function populateShipView(shipData) {
+    removeChildren(shipView)
+    let shipNum = getLastNumber(shipData.url)
     let shipImage = document.createElement('img')
-    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/9.jpg`
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+    shipImage.addEventListener('error', (err) => {
+        console.log(`Oops! Image doesn't exist.`)
+        shipImage.hidden = true
+    })
     shipView.appendChild(shipImage)
 }
+
+function addStarField(element, numStars) {
+    element.style.setProperty('background-color', '#000')
+}
+
+addStarField(document.querySelector('body'), 1000)
 
 populateNav(starships)
