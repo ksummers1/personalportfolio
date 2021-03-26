0b289c01b7170c4abe044734705056a9b1b05627
Kiera Summers <10262861@my.uvu.edu>
2021-03-26 04:03:31 -0600

    adding style to design;

--- starships/starships.js
+++ starships/starships.js
@@ -1,9 +1,10 @@
 import { starships } from '../data/starships.js'
 
-console.log(startship.length)
+console.log(starships.length)
 
 const nav = document.querySelector('nav')
 const navList = document.querySelector('.navList')
+const shipView = document.querySelector('.shipView')
 
 function populateNav(starships) {
     starships.forEach(starship => {
@@ -13,6 +14,12 @@ function populateNav(starships) {
         navList.appendChild(listItem)
     })
 
+}
+
+function populateShipView(shipData) {
+    let shipImage = document.createElement('img')
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/9.jpg`
+    shipView.appendChild(shipImage)
 }
 
 populateNav(starships)
