27de48b94ec6cf2051dc086312f1a46d39f3b2c6
Kiera Summers <10262861@my.uvu.edu>
2021-04-22 01:05:39 -0600

    changed size of pokemon pictures

--- pokemon/pokemoncode.js
+++ pokemon/pokemoncode.js
@@ -1,34 +1,51 @@
-const pokeGrid = document.querySelector('.pokeGrid')
-const loadButton = document.querySelector('.loadPokemon')
+const pokeGrid = document.querySelector('.pokeGrid');
+const loadButton = document.querySelector('.loadPokemon');
 
 loadButton.addEventListener('click', () => {
-    loadPage()
-})
+    loadPage();
+});
 
 async function getAPIData(url) {
     try {
-        const response = await fetch(url) //try getting data from API at the url
-        const data = await response.json() //convert the response into JSON
-        return data //return the data from the function to whatever called it
+        const response = await fetch(url); //try getting data from API at the url
+        const data = await response.json(); //convert the response into JSON
+        return data; //return the data from the function to whatever called it
     } catch (error) {
         //must have been an error
-        console.log(error)
+        console.log(error);
     }
 }
 
 function loadPage() {
-    getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25`).then(
-        (data) => {
-
-            for (const singlePokemon of data.results) {
-                console.log(singlePokemon)
-                populatePokeCard(singlePokemon)
-            }
+    getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25`).then((data) => {
+        for (const singlePokemon of data.results) {
+            console.log(singlePokemon);
+            populatePokeCard(singlePokemon);
         }
-
-    )
+    });
 }
 
 function populatePokeCard(singlePokemon) {
-    console.log(singlePokemon)
+    console.log(singlePokemon);
+    let pokeScene = document.createElement('div');
+    pokeScene.className = 'scene';
+    let pokeCard = document.createElement('div');
+    pokeCard.className = 'card';
+
+    pokeCard.appendChild(populateCardFront(singlePokemon));
+    pokeScene.appendChild(pokeCard);
+    pokeGrid.appendChild(pokeScene);
+}
+
+function populateCardFront(pokemon) {
+    let pokeFront = document.createElement('div');
+    pokeGrid.className = 'card_face card_face--front';
+    let frontLabel = document.createElement('p');
+    frontLabel.textContent = pokemon.name;
+    let frontImage = document.createElement('img');
+    frontImage.src = 'images/001.png';
+
+    pokeFront.appendChild(frontLabel);
+    pokeFront.appendChild(frontImage);
+    return pokeFront;
 }
