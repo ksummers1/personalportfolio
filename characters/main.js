d96f98b71a0308ae26734ae3f4dccf5027a36c74
Kiera Summers <10262861@my.uvu.edu>
2021-05-04 09:28:52 -0600

    update star wars

--- characters/main.js
+++ characters/main.js
@@ -49,6 +49,30 @@ function populateDOM(characters) {
         const charCaption = document.createElement('figcaption')
         charCaption.textContent = person.name
 
+        function addStarField(element, numStars) {
+            element.style.setProperty('background-color', 'black')
+            for (let i = 0; i < numStars; i++) {
+                let star = document.createElement('div')
+                star.style.setProperty('position', 'absolute')
+                star.style.setProperty('width', '3px')
+                star.style.setProperty('height', '3px')
+                star.style.setProperty('background-color', 'white')
+                let xy = getRandomPosition()
+                star.style.left = `${xy[0]}px`
+                star.style.top = `${xy[1]}px`
+                element.appendChild(star)
+
+            }
+        }
+
+        function getRandomPosition() {
+            let y = document.body.scrollHeight
+            let x = document.body.scrollWidth
+            let randomY = Math.floor(Math.random() * y)
+            let randomX = Math.floor(Math.random() * x)
+            return [randomX, randomY]
+        }
+
         charFigure.appendChild(charImg)
         charFigure.appendChild(charCaption)
 
